30fb246fc41ab0d268ba562110628829f1374c3b
SAAAM485 <143632382+SAAAM485@users.noreply.github.com>
2024-11-14 18:21:11 +0800

    Initial commit

---
+++ src/components/Board.jsx
@@ -0,0 +1,13 @@
+export default function Board() {
+    return (
+        <>
+            <header>
+                <h1>Genshin Memory Card</h1>
+                <div>Highest Score</div>
+                <div>Score</div>
+            </header>
+            <div className="card-container"></div>
+            <footer>A Fan-made Genshin Impact Memory Card Game</footer>
+        </>
+    );
+}
---
+++ src/components/Card.jsx
@@ -0,0 +1,10 @@
+export default function Card({ character }) {
+    return (
+        <div className="card">
+            <div className="card-content">
+                <img src="" alt="" />
+                <h1>{`${character}`}</h1>
+            </div>
+        </div>
+    );
+}
